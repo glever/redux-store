fdb1046990d334794758b212809d5b44e80bb161
Glen Vermeylen <glen.vermeylen@fednot.be>
2019-04-18 10:29:34 +0200

    add subscriber and unsubscriber manipulating ui

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,45 @@
-import * as fromStore from './store';
+import * as fromStore from "./store";
+import {renderTodos} from "./utils";
 
-const input = document.querySelector('input') as HTMLInputElement;
-const button = document.querySelector('button') as HTMLButtonElement;
-const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
-const todoList = document.querySelector('.todos') as HTMLLIElement;
+const input = document.querySelector("input") as HTMLInputElement;
+const button = document.querySelector("button") as HTMLButtonElement;
+const destroy = document.querySelector(".unsubscribe") as HTMLButtonElement;
+const todoList = document.querySelector(".todos") as HTMLLIElement;
 
 const reducers = {
   todos: fromStore.reducer
 };
 const store = new fromStore.Store(reducers);
 
-
-
 button.addEventListener(
-  'click',
+  "click",
   () => {
     if (!input.value.trim()) return;
 
     const payload = { label: input.value, complete: false };
 
     store.dispatch({
-      type: 'ADD_TODO',
+      type: "ADD_TODO",
       payload
     });
 
     console.log(store.value);
 
-    input.value = '';
+    input.value = "";
   },
   false
 );
 
-todoList.addEventListener('click', function(event) {
+const unsubscribe = store.subscribe(state =>{
+  renderTodos(state.todos.data);
+});
+
+destroy.addEventListener('click', unsubscribe, false);
+todoList.addEventListener("click", function(event) {
   const target = event.target as HTMLButtonElement;
-  if (target.nodeName.toLowerCase() === 'button') {
+  if (target.nodeName.toLowerCase() === "button") {
     console.log(target);
   }
 });
+
+store.subscribe(state => console.log("STATE:::", state));
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,26 +1,43 @@
-export class Store{
-  private subscribers: Function [];
-  private reducers: {[key: string]: Function};
-  private state: {[key: string]: any};
+export class Store {
+  private subscribers: Function[];
+  private reducers: { [key: string]: Function };
+  private state: { [key: string]: any };
 
-  constructor(reducers = {}, initialState= {}){
+  constructor(reducers = {}, initialState = {}) {
+    this.subscribers = [];
     this.reducers = reducers;
     this.state = this.reduce(initialState, {});
   }
 
-  get value(){
+  get value() {
     return this.state;
+  }
+
+  /*
+  Adds a subscriber. Returns a function that, when called. Removes the subscriber.
+   */
+  subscribe(fn) {
+    this.subscribers = [...this.subscribers, fn];
+    this.notify();
+    return () => {
+      this.subscribers = this.subscribers.filter(sub => sub !== fn);
+    };
   }
 
   dispatch(action) {
     this.state = this.reduce(this.state, action);
+    this.notify();
+  }
+
+  private notify() {
+    this.subscribers.forEach(fn => fn(this.value));
   }
 
   private reduce(state, action) {
     const newState = {};
 
-    for(const prop in this.reducers){
-      newState[prop]= this.reducers[prop](state[prop], action);
+    for (const prop in this.reducers) {
+      newState[prop] = this.reducers[prop](state[prop], action);
     }
 
     return newState;
