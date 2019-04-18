f9af69729a045c7d33bd5408adacf43b1d9765db
Glen Vermeylen <glen.vermeylen@fednot.be>
2019-04-18 10:50:31 +0200

    add remove todo reducer

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import * as fromStore from "./store";
-import {renderTodos} from "./utils";
+import { renderTodos } from "./utils";
 
 const input = document.querySelector("input") as HTMLInputElement;
 const button = document.querySelector("button") as HTMLButtonElement;
@@ -16,29 +16,25 @@ button.addEventListener(
   () => {
     if (!input.value.trim()) return;
 
-    const payload = { label: input.value, complete: false };
+    const todo = { label: input.value, complete: false };
 
-    store.dispatch({
-      type: "ADD_TODO",
-      payload
-    });
-
-    console.log(store.value);
+    store.dispatch(new fromStore.AddTodo(todo));
 
     input.value = "";
   },
   false
 );
 
-const unsubscribe = store.subscribe(state =>{
+const unsubscribe = store.subscribe(state => {
   renderTodos(state.todos.data);
 });
 
-destroy.addEventListener('click', unsubscribe, false);
+destroy.addEventListener("click", unsubscribe, false);
 todoList.addEventListener("click", function(event) {
   const target = event.target as HTMLButtonElement;
   if (target.nodeName.toLowerCase() === "button") {
-    console.log(target);
+    const todo = JSON.parse(target.getAttribute("data-todo") as any);
+    store.dispatch(new fromStore.RemoveTodo(todo));
   }
 });
 
--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,19 +1,27 @@
+import * as fromActions from "./actions";
+
 export const initialState = {
   loaded: false,
   loading: false,
-  data: [{label: 'Eat pizza', complete: false}],
+  data: [{ label: "Eat pizza", complete: false }]
 };
 
 export function reducer(
   state = initialState,
-  action: { type: string, payload: any }
+  action: { type: string; payload: any }
 ) {
-
   switch (action.type) {
-    case 'ADD_TODO': {
+    case fromActions.ADD_TODO: {
       const todo = action.payload;
       const data = [...state.data, todo];
-      return {...state, data}
+      return { ...state, data };
+    }
+
+    case fromActions.REMOVE_TODO: {
+      const data = state.data.filter(
+        todo => todo.label !== action.payload.label
+      );
+      return { ...state, data };
     }
   }
   return state;
